5e8ff6fe8488d8eb29b56e3b7413cff5b7d2efdb
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-16 00:57:30 +0330

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter a y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int b = Math.Abs(y);
+            int c = Math.Abs(x - y);
+            Console.Write(Math.Sqrt(c * b));
+        }
+    }
+}
